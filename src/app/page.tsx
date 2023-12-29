6fba5d5c0513e471992ae151b2fb7567ed3a0e37
vonrie <viscaynovonrie@gmail.com>
2023-12-29 23:32:34 +0800

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <Image
         src="/bsm-logo-square.png"
         alt="Bestseller Meatshop Logo"
