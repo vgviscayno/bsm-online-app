617cc50eda0cf47cd465dfb2d14b4692127c4360
vonrie <viscaynovonrie@gmail.com>
2024-04-28 18:25:46 +0800

    add to card form

---
+++ src/components/add-to-cart-dialog.tsx
@@ -0,0 +1,143 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export default function AddToCartDialog({ unit }: { unit: string }) {
+  // form
+  // quantity (plus, minus)
+  // notes (for special requests)
+  const [open, setOpen] = useState(false);
+  const isDesktop = window?.innerWidth > 768 ?? true;
+
+  useEffect(() => {
+    function handleResize() {
+      // if window is resized, just close both dialog and drawer (?)
+      setOpen(false);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="w-full">
+            Add to Cart
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          {/* Form here */}
+          <FillItemDetailsForm unit={unit} />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="outline" className="w-full">
+          Add to Cart
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>Edit profile</DrawerTitle>
+          <DrawerDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </DrawerDescription>
+        </DrawerHeader>
+        {/* Form here */}
+        <FillItemDetailsForm unit={unit} />
+        <DrawerFooter className="pt-2">
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+const formSchema = z.object({
+  quantity: z.number().min(0.25).max(10),
+});
+
+function FillItemDetailsForm({ unit }: { unit: string }) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      quantity: 1,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log({ values });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="quantity"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Quantity</FormLabel>
+              <FormControl>
+                <Input {...field} type="number" size={0.25} />
+              </FormControl>
+              <span>{unit}</span>
+              <FormDescription>Number of {unit} for this item</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Add to Cart</Button>
+      </form>
+    </Form>
+  );
+}
--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,9 +1,8 @@
+import AddToCartDialog from "@/components/add-to-cart-dialog";
 import ProductCardPlaceholder from "@/components/product-card-image-placeholder";
-import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { productTable } from "@/db/schema/products";
 import Image from "next/image";
-import Link from "next/link";
 
 type Props = {
   imageSrc?: string;
@@ -12,38 +11,37 @@ type Props = {
 
 export default function ProductCard({ imageSrc, product }: Props) {
   return (
-    <Link href="/asdjioasjdaiosdjio">
-      <article>
-        <Card className="w-full max-w-xs mx-auto rounded-xl border cursor-pointer translate-y-0 hover:translate-y-[-2px] transition-transform">
-          <div className="grid gap-4 p-4">
-            <div className="aspect-square w-full overflow-hidden rounded-lg">
-              {imageSrc ? (
-                <Image
-                  alt="Product Image"
-                  className="aspect-square object-cover border border-gray-200 dark:border-gray-800"
-                  height={600}
-                  src="/placeholder.svg"
-                  width={600}
-                />
-              ) : (
-                <ProductCardPlaceholder stroke="#fe5858" />
-              )}
-            </div>
-            <div className="text-base font-bold">{product.name}</div>
-            <div className="text-sm">
-              {new Intl.NumberFormat("en-PH", {
-                style: "currency",
-                currency: "PHP",
-              }).format(product.price!)}
-            </div>
+    <article>
+      <Card className="w-full max-w-xs mx-auto rounded-xl border cursor-pointer translate-y-0 hover:translate-y-[-2px] transition-transform">
+        <div className="grid gap-4 p-4">
+          <div className="aspect-square w-full overflow-hidden rounded-lg">
+            {imageSrc ? (
+              <Image
+                alt="Product Image"
+                className="aspect-square object-cover border border-gray-200 dark:border-gray-800"
+                height={600}
+                src="/placeholder.svg"
+                width={600}
+              />
+            ) : (
+              <ProductCardPlaceholder stroke="#fe5858" />
+            )}
           </div>
-          <div className="p-4 flex items-center justify-center gap-4">
-            <Button variant="outline" className="w-full">
-              Add to cart
-            </Button>
+          <div className="text-base font-bold">{product.name}</div>
+          <div className="text-sm">
+            {new Intl.NumberFormat("en-PH", {
+              style: "currency",
+              currency: "PHP",
+            }).format(product.price!)}
           </div>
-        </Card>
-      </article>
-    </Link>
+        </div>
+        <div className="p-4 flex items-center justify-center gap-4">
+          {/* <Button variant="outline" className="w-full">
+          Add to cart
+        </Button> */}
+          <AddToCartDialog unit={product.unit || "units"} />
+        </div>
+      </Card>
+    </article>
   );
 }
